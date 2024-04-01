4976201dc655949aa2b0a795221d2e02b456d99e
Marcos Duarte <pe.marcos30@gmail.com>
2024-04-01 20:23:55 -0300

    Adicionando integracao Tela Inicial

--- components/TelaInicial.js
+++ components/TelaInicial.js
@@ -1,20 +1,113 @@
-import React from 'react';
-import { View, Image, Text, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Image, Text, Button, StyleSheet } from "react-native";
 import NavBar from "./NavBar";
+import { CameraView, Camera } from "expo-camera/next";
+import axios from "axios";
 
 const TelaInicial = ({ navigation }) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [abrirCamera, setAbrirCamera] = useState(false);
+  const [name, setName] = useState("BEM VINDO!");
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const renderCamera = () => {
+    <View style={styles.cameraContainer}>
+      <CameraView
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        barCodeScannerSettings={{
+          barCodeTypes: ["qr", "pdf417"],
+        }}
+        style={StyleSheet.absoluteFillObject}
+      />
+      ;
+    </View>;
+  };
+
+  const handleBarCodeScanned = async ({ type, data }) => {
+    setScanned(true);
+    // setAbrirCamera(false);
+
+    const url = `https://quicktable-back.onrender.com/reservas/entrarNaReserva/${data}`;
+
+    navigation.navigate("Cardapio");
+
+    /*try {
+      const response = await axios.post(url);
+
+      if (response.status == 200) {
+        navigation.navigate("Cardapio");
+      } else {
+        console.error("Erro ao entrar na reserva");
+        console.error(response.status);
+      }
+    } catch (error) {
+      console.error(error);
+    }*/
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Camera permission not granted</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <NavBar navigation={navigation} nome="BEM VINDO!" />
+      {abrirCamera ? (
+        <CameraView
+          onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+          barCodeScannerSettings={{
+            barCodeTypes: ["qr", "pdf417"],
+          }}
+          style={StyleSheet.absoluteFillObject}
+        />
+      ) : null}
+
+      <NavBar navigation={navigation} nome={name} />
+
       <View style={styles.content}>
-        <Image source={require('../Content/logo.png')} style={styles.logo} />
-        <View style={styles.buttonsContainer}>
-          <Button title="Ler QR Code" style={styles.button} />
-          <Button title="Cardápio" style={styles.button} onPress={() => navigation.navigate('Cardapio')} />
-        </View>
-        <View style={styles.loginButtonContainer}>
-          <Button title="Login" style={styles.loginButton} />
-        </View>
+        {!abrirCamera ? (
+          <Image source={require("../Content/logo.png")} style={styles.logo} />
+        ) : null}
+
+        {!abrirCamera ? (
+          <View style={styles.buttonsContainer}>
+            <Button
+              title="Ler QR Code"
+              style={styles.button}
+              onPress={() => {
+                setScanned(false);
+                setAbrirCamera(true);
+                setName("LEIA UM QR CODE");
+              }}
+            />
+            <Button
+              title="Cardápio"
+              style={styles.button}
+              onPress={() => navigation.navigate("Cardapio")}
+            />
+          </View>
+        ) : null}
+
+        {!abrirCamera ? (
+          <View style={styles.loginButtonContainer}>
+            <Button title="Login" style={styles.loginButton} />
+          </View>
+        ) : null}
       </View>
     </View>
   );
@@ -24,41 +117,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#091014",
-    display: 'flex',
+    display: "flex",
   },
   content: {
     padding: 20,
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 50,
   },
   titulo: {
     fontSize: 32,
-    fontWeight: 'bold',
-    color: 'orange',
+    fontWeight: "bold",
+    color: "orange",
     marginBottom: 40,
   },
   buttonsContainer: {
-    flexDirection: 'row-wrap',
-    justifyContent: 'space-around',
+    flexDirection: "row-wrap",
+    justifyContent: "space-around",
     height: 80,
   },
   button: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 10,
     borderRadius: 5,
     margin: 10,
   },
   loginButtonContainer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
     right: 20,
   },
   logo: {
-    width: '100%',
-    height: '30%',
+    width: "100%",
+    height: "30%",
     marginBottom: 30,
+  },
+  cameraContainer: {
+    width: "80%",
+    aspectRatio: 1,
+    overflow: "hidden",
+    borderRadius: 10,
+    marginBottom: 40,
   },
 });
 
